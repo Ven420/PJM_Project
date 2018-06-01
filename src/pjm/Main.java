bae88939ef001ecd94e6f6a1c6d102a9ed3cfa7c
svwei <svwei@DESKTOP-HPVS2IN.fritz.box>
2018-06-01 16:25:45 +0200

    Aufräumen der Entwicklungsumgebung

--- src/pjm/Main.java
+++ src/pjm/Main.java
@@ -1,8 +1,8 @@
-package Timerbtn;
+package pjm;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application
@@ -11,12 +11,9 @@ public class Main extends Application
    @Override
    public void start(Stage primaryStage)
    {
-      Box vBox = new Box();
-      vBox.addTimerButtonV(new TimerButton(10));
-      vBox.addTimerButtonV(new TimerButton(10));
-      vBox.addButtonV(new Button());
+      BorderPane root = new BorderPane();
 
-      Scene scene = new Scene(vBox.getVBox());
+      Scene scene = new Scene(root);
       primaryStage.setTitle("Card Layout");
       primaryStage.setScene(scene);
       primaryStage.show();
